8c511a9b9f7554f931bcae8fc1a793dfe6ef5762
Liviu Chis <chis@adobe.com>
2025-11-18 16:32:39 -0700

    Moved svg files

--- blocks/grocery-carousel/grocery-carousel.js
+++ blocks/grocery-carousel/grocery-carousel.js
@@ -9,10 +9,6 @@ export default async function decorate(block) {
     <div style="padding: 20px; border: 2px solid #333;">
       <h2>Grocery Carousel Test</h2>
       <p>If you can see this, the block is loading successfully!</p>
-      <p>Block class: ${block.className}</p>
-      <img src="${window.hlx.codeBasePath}/icons/cart-icon.svg" alt="Cart" width="48" height="48" />
-      <img src="${window.hlx.codeBasePath}/icons/heart-icon.svg" alt="Heart" width="48" height="48" />
-      <img src="${window.hlx.codeBasePath}/icons/canada-flag.svg" alt="Canada" width="48" height="48" />
     </div>
   `;
   
